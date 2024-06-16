a7673d599b9c28c95a8da066ec00833fa3219632
Ajinkya <patilajinkya5683@gmail.con>
2024-06-16 17:15:11 +0530

    second copmmit

--- seat11.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const seats = document.querySelectorAll('.seat.available');
-
-    seats.forEach(seat => {
-        seat.addEventListener('click', () => {
-            seat.classList.toggle('selected');
-        });
-    });
-});
